f27516ba766556b41c068606410e134408adeb50
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2026-02-18 10:23:18 -0500

    Try different browser open vector

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -6,6 +6,18 @@ return {
   build = function() vim.fn["mkdp#util#install"]() end,
   -- set globals before the plugin loads
   init = function()
+    vim.cmd([[
+      function! MkdpOpenSystemDefault(url) abort
+        if has('mac')
+          call jobstart(['open', a:url], {'detach': v:true})
+        elseif has('win32')
+          call jobstart(['cmd.exe', '/c', 'start', '', a:url], {'detach': v:true})
+        else
+          call jobstart(['xdg-open', a:url], {'detach': v:true})
+        endif
+      endfunction
+    ]])
+    vim.g.mkdp_browserfunc = "MkdpOpenSystemDefault"
     vim.g.mkdp_echo_preview_url = 1
     vim.g.mkdp_port = 0
     vim.g.mkdp_filetypes = { "markdown" }
