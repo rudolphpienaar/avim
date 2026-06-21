60fb133e5c4642fa667c2b8eb4e8ed83a3316780
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2026-06-21 16:20:56 +0200

    treesitter: drop latex parser (build fails on termux; vimtex handles tex)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,14 +8,7 @@ return {
       "lua",
       "vim",
       "python",
-      "latex",   -- parser for structure/folding (highlight disabled below)
       "bibtex",  -- .bib bibliography files
-    },
-    highlight = {
-      -- Disable treesitter highlighting for latex — the bundled highlight
-      -- queries use nodes (curly_group_text) not present in the installed
-      -- parser. Vimtex provides better latex syntax highlighting anyway.
-      disable = { "latex" },
     },
   },
 }
